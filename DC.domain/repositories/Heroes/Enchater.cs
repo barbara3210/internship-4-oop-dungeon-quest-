3a363ed94423fc6cde7389ee9b2a7e086573ff37
barbara3210 <bivkovic@pmfst.hr>
2023-12-04 00:30:51 +0100

    encharter_attack

--- DC.domain/repositories/Heroes/Enchater.cs
+++ DC.domain/repositories/Heroes/Enchater.cs
@@ -11,7 +11,7 @@ namespace DungeonCrawler.Domain.repositories.Heroes
 {
     public class Enchater : Hero
     {
-        //■	Ima najmanji HP i najveći damage
+        
         public int Mana { get; private set; }
 
         public Enchater(string player, int hp, int damage, int xp)
@@ -20,18 +20,43 @@ namespace DungeonCrawler.Domain.repositories.Heroes
             HealthPoints = 30;
             Damage = 80;
 
-            Mana = 30;
+            Mana = 100;
         }
         public override void Attack(Monster monster)
         {
-            base.Attack(monster);
+            if (Mana > 0)
+            {
+                base.Attack(monster);
+                Mana -= monster.HealthPoints;
+            }
+            else
+            {
+                Console.WriteLine("No more mana");
+                Mana = 100;
+            }
+            
+        }
+
+        public void UseManaToHeal(int manaToUse)
+        {
+            if (Mana >= manaToUse)
+            {
+                HealthPoints += Mana;
+                Mana -= manaToUse;
+                Console.WriteLine($"Used {manaToUse} mana to restore HP");
+            }
+            else
+            {
+                Console.WriteLine("Not enough mana to perform this action.");
+            }
         }
 
         public override void LevelUp()
         {
-
+            
             HealthPoints += 10;
             Damage += 10;
+            Mana += 10;
         }
     }
 }
